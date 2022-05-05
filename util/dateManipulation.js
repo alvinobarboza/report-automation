5d5b3daab816322e9870f56ac6de191a86d8bd2b
alvino.barboza <alvinobarboza@gmail.com>
2022-05-05 15:34:54 -0300

    Added two new methods

--- util/dateManipulation.js
+++ util/dateManipulation.js
@@ -2,6 +2,14 @@ const getDate = ()=>{
     return (new Date()).toLocaleDateString('pt-br');
 }
 
+const getCurrentMonthYearNumeric = () => {
+    const tempDate = new Date();
+    return (new Date(tempDate.getFullYear(), tempDate.getMonth()))
+        .toLocaleString('pt-BR', {month: 'numeric', year: 'numeric'})
+        .split('/')
+        .join('.');
+}
+
 const getCurrentMonth = () => {
 	return (new Date).toLocaleString('pt-BR', {month: 'short'})
 		.toLocaleUpperCase()
@@ -17,6 +25,14 @@ const getLastMonthTimestamp = () => {
 		.getMonth() - 1))).getTime();
 }
 
+const getCurrentMonthYearShort = () => {
+    const tempDate = new Date();
+    const date = (new Date(tempDate.getFullYear(), tempDate.getMonth()))
+        .toLocaleString('pt-BR', {month: 'short', year: '2-digit'})
+        .toLocaleLowerCase();
+    return date.substring(0,3) +'/'+ date.substring(date.length-2);
+}
+
 const convertTimestampToMonthAndYear = (timestamp) => {
 	const year = (new Date(timestamp)).getFullYear();
 	const month = (new Date(timestamp)).toLocaleString('pt-BR', {month: 'short'}).toLocaleUpperCase().split('.')[0];
@@ -25,8 +41,10 @@ const convertTimestampToMonthAndYear = (timestamp) => {
 
 module.exports = {
     getCurrentMonth,
+	getCurrentMonthYearNumeric,
     getCurrentYear,
     getDate,
     getLastMonthTimestamp,
+	getCurrentMonthYearShort,
     convertTimestampToMonthAndYear
 }
