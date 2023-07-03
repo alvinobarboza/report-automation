f991cfcf1acef88ce4fe24b49baf40e9e13f76c8
Alvino Barboza <alvinobarboza@gmail.com>
2023-07-03 16:33:16 -0300

    Novos reports de SVA

--- index.js
+++ index.js
@@ -1,76 +1,96 @@
-const { groupByDealerByCustomer, groupByGeneric } = require("./util/groupByFunctions");
-const { writeFile } = require("./util/writeToExcelFileFunctions");
-const { getReport, getToken } = require("./util/moTVCalls");
+const {
+    groupByDealerByCustomer,
+    groupByGeneric,
+} = require('./util/groupByFunctions');
+const { writeFile } = require('./util/writeToExcelFileFunctions');
+const { getReport, getToken } = require('./util/moTVCalls');
 require('dotenv').config();
-const { smsBody, smsHeader, REPORT, SMSURL, MWURL, mwBody, mwHeader, TELEMEDICINA_QUERY, } = require("./util/constants");
-const { validation } = require("./util/packageValidationFunctions");
-const { validationUrban } = require("./util/urbanValidation");
+const {
+    smsBody,
+    smsHeader,
+    REPORT,
+    SMSURL,
+    MWURL,
+    mwBody,
+    mwHeader,
+    TELEMEDICINA_QUERY,
+} = require('./util/constants');
+const { validation } = require('./util/packageValidationFunctions');
+const { validationUrban } = require('./util/urbanValidation');
 
-const pool = require("./util/dbconnection.js");
-const { validateTelemedicinaData } = require("./util/validateTelemedicinaCustomer");
-const { accessLog } = require("./util/logs");
-const telemedicina = pool.query(TELEMEDICINA_QUERY);
+const pool = require('./util/dbconnection.js');
+const { validateSvaData } = require('./util/validateSvaCustomer');
+const { accessLog } = require('./util/logs');
 
-const LOGINSMS = process.env.loginSMS
-const SECRETSMS = process.env.secretSMS
+const TELEMEDICINA = 'TELEMEDICINA';
+const FITANYWHERE = 'FITANYWHERE';
+const BIBLIOTECHIE = 'BIBLIOTECHIE';
+const telemedicina = pool.query(TELEMEDICINA_QUERY, [TELEMEDICINA]);
+const fitanywhere = pool.query(TELEMEDICINA_QUERY, [FITANYWHERE]);
+const bibliotechie = pool.query(TELEMEDICINA_QUERY, [BIBLIOTECHIE]);
 
-const LOGINMW = process.env.loginMW
-const SECRETMW = process.env.secretMW
+const LOGINSMS = process.env.loginSMS;
+const SECRETSMS = process.env.secretSMS;
+
+const LOGINMW = process.env.loginMW;
+const SECRETMW = process.env.secretMW;
 
 const REPORTBRAND = 119;
 const REPORTDEALERS = 126;
 const REPORTACTIVECUSTOMERS = 118;
 const REPORTURBANTV = 88;
 const TELEMEDICINAREPORT = 263;
+const FITANYWHEREREPORT = 286;
+const BIBLIOTECHIEREPORT = 285;
 
-const getTelemedicinaData = () => getReport(
-    SMSURL + REPORT,
-    smsBody(TELEMEDICINAREPORT),
-    smsHeader(
-        getToken(
-            LOGINSMS,
-            SECRETSMS
-        )
-    )
-);
+const getFitanywhereData = () =>
+    getReport(
+        SMSURL + REPORT,
+        smsBody(FITANYWHEREREPORT),
+        smsHeader(getToken(LOGINSMS, SECRETSMS))
+    );
 
-const getActiveCustomers = () => getReport(
-    MWURL + REPORT,
-    mwBody(REPORTACTIVECUSTOMERS),
-    mwHeader(
-        getToken(LOGINMW, SECRETMW)
-    )
-);
+const getBibliotechieData = () =>
+    getReport(
+        SMSURL + REPORT,
+        smsBody(BIBLIOTECHIEREPORT),
+        smsHeader(getToken(LOGINSMS, SECRETSMS))
+    );
 
-const getUrbanReports = () => getReport(
-    MWURL + REPORT,
-    mwBody(REPORTURBANTV),
-    mwHeader(
-        getToken(LOGINMW, SECRETMW)
-    )
-);
+const getTelemedicinaData = () =>
+    getReport(
+        SMSURL + REPORT,
+        smsBody(TELEMEDICINAREPORT),
+        smsHeader(getToken(LOGINSMS, SECRETSMS))
+    );
 
-const getCustomersData = () => getReport(
-    SMSURL + REPORT,
-    smsBody(REPORTBRAND),
-    smsHeader(
-        getToken(
-            LOGINSMS,
-            SECRETSMS
-        )
-    )
-);
+const getActiveCustomers = () =>
+    getReport(
+        MWURL + REPORT,
+        mwBody(REPORTACTIVECUSTOMERS),
+        mwHeader(getToken(LOGINMW, SECRETMW))
+    );
 
-const getDealersData = () => getReport(
-    SMSURL + REPORT,
-    smsBody(REPORTDEALERS),
-    smsHeader(
-        getToken(
-            LOGINSMS,
-            SECRETSMS
-        )
-    )
-);
+const getUrbanReports = () =>
+    getReport(
+        MWURL + REPORT,
+        mwBody(REPORTURBANTV),
+        mwHeader(getToken(LOGINMW, SECRETMW))
+    );
+
+const getCustomersData = () =>
+    getReport(
+        SMSURL + REPORT,
+        smsBody(REPORTBRAND),
+        smsHeader(getToken(LOGINSMS, SECRETSMS))
+    );
+
+const getDealersData = () =>
+    getReport(
+        SMSURL + REPORT,
+        smsBody(REPORTDEALERS),
+        smsHeader(getToken(LOGINSMS, SECRETSMS))
+    );
 
 // async function main() {
 //     const customers = require('./out/FEV2023_2023-02-27/raw_FEV2023_2023-02-21.json');
@@ -86,21 +106,46 @@ const getDealersData = () => getReport(
 
 // main();
 
-
-(async function (){
+(async function () {
     try {
         const customers = await getCustomersData();
         const dealers = await getDealersData();
         const activeCustomers = await getActiveCustomers();
-        const urban = await getUrbanReports()
+        const urban = await getUrbanReports();
         const telemedicinaActive = await telemedicina;
+        const bibliotechieActive = await bibliotechie;
+        const fitanywhereActive = await fitanywhere;
+
         const telemedicinaSubscribed = await getTelemedicinaData();
-    
-        const telemedicinaValidatedData = validateTelemedicinaData(telemedicinaActive.rows, telemedicinaSubscribed.response.rows);
+        const fitanywhereSubscribed = await getFitanywhereData();
+        const bibliotechieSubscribed = await getBibliotechieData();
+
+        const telemedicinaValidatedData = validateSvaData(
+            telemedicinaActive.rows,
+            telemedicinaSubscribed.response.rows
+        );
+        const fitanywhereValidatedData = validateSvaData(
+            fitanywhereActive.rows,
+            fitanywhereSubscribed.response.rows
+        );
+        const bibliotechieValidatedData = validateSvaData(
+            bibliotechieActive.rows,
+            bibliotechieSubscribed.response.rows
+        );
+
+        const svas = [
+            { data: telemedicinaValidatedData, sva: TELEMEDICINA },
+            { data: fitanywhereValidatedData, sva: FITANYWHERE },
+            { data: bibliotechieValidatedData, sva: BIBLIOTECHIE },
+        ];
         const groupedData = groupByDealerByCustomer(customers.response.rows);
         const validatedUrban = validationUrban(urban.response.rows);
-        const { newPackaging, oldPackaging } = await validation(groupedData, activeCustomers.response.rows, dealers.response.rows);
-        console.log(oldPackaging);
+        const { newPackaging, oldPackaging } = await validation(
+            groupedData,
+            activeCustomers.response.rows,
+            dealers.response.rows
+        );
+        // // console.log(oldPackaging);
         writeFile(
             customers.response.rows,
             oldPackaging,
@@ -108,45 +153,10 @@ const getDealersData = () => getReport(
             dealers.response.rows,
             validatedUrban,
             urban.response.rows,
-            telemedicinaValidatedData
-        );        
+            svas
+        );
     } catch (error) {
         console.log(error);
         accessLog(error.stack);
     }
-})()
-
-// Promise.all([
-//     getCustomersData(),
-//     getDealersData(),
-//     getActiveCustomers(),
-//     getUrbanReports(),
-//     telemedicina,
-//     getTelemedicinaData()
-// ]).then(async (result) => {
-//     const [
-//             customers,
-//             dealers,
-//             activeCustomers,
-//             urban,
-//             telemedicinaActive,
-//             telemedicinaSubscribed
-//     ] = result;
-
-//     const telemedicinaValidatedData = validateTelemedicinaData(telemedicinaActive.rows, telemedicinaSubscribed.response.rows);
-//     writeFile(telemedicinaValidatedData);
-//     const groupedData = groupByDealerByCustomer(customers.response.rows);
-//     const validatedUrban = validationUrban(urban.response.rows);
-//     const { newPackaging, oldPackaging } = await validation(groupedData, activeCustomers.response.rows, dealers.response.rows);
-//     console.log(oldPackaging);
-//     writeFile(
-//         customers.response.rows,
-//         oldPackaging,
-//         newPackaging,
-//         dealers.response.rows,
-//         validatedUrban,
-//         urban.response.rows
-//     );
-// }).catch((err) => {
-//     console.log(err);
-// });
+})();
