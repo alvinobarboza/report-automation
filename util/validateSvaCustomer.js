6647328d3528e9a1c4ea62374956605b08bf046d
Alvino Barboza <alvinobarboza@gmail.com>
2023-07-03 16:32:12 -0300

    Transformado em método genéricos

--- util/validateSvaCustomer.js
+++ util/validateSvaCustomer.js
@@ -1,79 +1,80 @@
-const { validateLoginTest } = require("./packageValidationFunctions");
+const { validateLoginTest } = require('./packageValidationFunctions');
 
-function validateTelemedicinaData(telemedicinaActive, telemedicinaSubscribed){
-    try{
-        const groupedCustomers = groupTelemedicinaUserFromMotvReport(telemedicinaActive, telemedicinaSubscribed);
-        return [groupedCustomers, telemedicinaSubscribed];
-    }catch(e){
+function validateSvaData(svaActive, svaSubscribed) {
+    try {
+        const groupedCustomers = groupSvaUserFromMotvReport(
+            svaActive,
+            svaSubscribed
+        );
+        return [groupedCustomers, svaSubscribed];
+    } catch (e) {
         console.log(e);
         return [];
     }
 }
 
-function groupTelemedicinaUserFromMotvReport(active, data){
+function groupSvaUserFromMotvReport(active, data) {
     const dealersGrouped = {};
     const notValidDealers = [
-         1, // 'JACON dealer'
-         3, // 'Admin'
-         5, // 'Youcast CSMS'
-         7, // 'Z-Não-usar'
-         13,  // 'Z-Não-usar'
-         22  // 'ADMIN-YOUCAST' 
-    ]
+        1, // 'JACON dealer'
+        3, // 'Admin'
+        5, // 'Youcast CSMS'
+        7, // 'Z-Não-usar'
+        13, // 'Z-Não-usar'
+        22, // 'ADMIN-YOUCAST'
+    ];
 
     console.time('group');
     for (const customer of data) {
-        if(notValidDealers.includes(customer.dealerid)){
+        if (notValidDealers.includes(customer.dealerid)) {
             continue;
         }
 
-        if(dealersGrouped[customer.dealer]){
+        if (dealersGrouped[customer.dealer]) {
             dealersGrouped[customer.dealer].customers.push(customer);
-        }else{
+        } else {
             dealersGrouped[customer.dealer] = {
                 dealer: customer.dealer,
                 dealerid: customer.dealerid,
-                customers: [customer]
-            }
-        }     
+                customers: [customer],
+            };
+        }
     }
-    
+
     const groupedDealersArray = Object.values(dealersGrouped);
-    
+
     for (const dealer of groupedDealersArray) {
         const customersGrouped = {};
         for (const customer of dealer.customers) {
-            if(validateLoginTest(customer)){
+            if (validateLoginTest(customer)) {
                 continue;
             }
 
-
             let status = false;
-            for(const user of active){
-                if(user.smsid == customer.idsms){
+            for (const user of active) {
+                if (user.smsid == customer.idsms) {
                     status = true;
                 }
             }
 
-
-            if(customersGrouped[customer.idsms]){
+            if (customersGrouped[customer.idsms]) {
                 customersGrouped[customer.idsms].status = status;
                 customersGrouped[customer.idsms].products.push(customer);
-            }else{
+            } else {
                 customersGrouped[customer.idsms] = {
                     idsms: customer.idsms,
                     login: customer.login,
                     dealer: customer.dealer,
                     status: status,
-                    products: [customer]
-                }
-            }     
+                    products: [customer],
+                };
+            }
         }
-        const customers = Object.values(customersGrouped)
+        const customers = Object.values(customersGrouped);
         dealer.customers = customers;
         dealer.subscribed = customers.length;
-        dealer.active = customers.reduce((amount, customer)=>{
-            if(customer.status){
+        dealer.active = customers.reduce((amount, customer) => {
+            if (customer.status) {
                 return amount + 1;
             }
             return amount;
@@ -84,6 +85,6 @@ function groupTelemedicinaUserFromMotvReport(active, data){
 }
 
 module.exports = {
-    validateTelemedicinaData,
-    groupTelemedicinaUserFromMotvReport
-}
+    validateSvaData,
+    groupSvaUserFromMotvReport,
+};
